910047e1cfedda89b9e74dd603c04208f176a710
unknown <omerhalely6@gmail.com>
2024-03-11 18:28:38 +0200

    Updated the readme file

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # WGAN
-Implementation of a WGAN
+Implementation of a WGAN.
+
+## Training
+For training a model use the following line:
+```bash
+python main.py --model "model" --model-name "model-name" --data "data-type" --epochs 100 --train True
+```
+model is the model which will be trained (WGAN/DCGAN)
+
+model-name is the name of the model which will be saved to ./models/model-name.
+
+data is the type of the data which will be used in the training process (CIFAR10/FashionMNIST).
+
+For example, training a WGAN over the CIFAR10 dataset:
+```bash
+python main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10" --epochs 100 --train True
+```
+
+## Testing
+```bash
+python main.py --model "model" --model-name "model-name" --data "data-type"
+```
+model is the model which will be trained (WGAN/DCGAN)
+
+model-name is the name of the model which will be saved to ./models/model-name.
+
+data is the type of the data which will be used in the training process (CIFAR10/FashionMNIST).
+
+For example, testing the WGAN which was trained on the CIFAR10 dataset:
+```bash
+python main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10"
+```
+
+Samples of the output images of the WGAN will eb saved to ./models/model-name
--- run_me.sh
+++ run_me.sh
@@ -1,20 +1,20 @@
 # Training WGAN on CIFAR10 dataset.
-python3 main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10" --epochs 100 --train true
+python main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10" --epochs 100 --train True
 # Testing WGAN_CIFAR10.
-python3 main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10"
+python main.py --model "WGAN" --model-name "WGAN_CIFAR10" --data "CIFAR10"
 
 # Training WGAN on FashionMNIST dataset.
-python3 main.py --model "WGAN" --model-name "WGAN_FashionMNIST" --data "FashionMNIST" --epochs 100 --train True
+python main.py --model "WGAN" --model-name "WGAN_FashionMNIST" --data "FashionMNIST" --epochs 100 --train True
 # Testing WGAN_FashionMNIST.
-python3 main.py --model "WGAN" --model-name "WGAN_FashionMNIST" --data "FashionMNIST"
+python main.py --model "WGAN" --model-name "WGAN_FashionMNIST" --data "FashionMNIST"
 
 
 # Training DCGAN on CIFAR10 dataset.
-python3 main.py --model "DCGAN" --model-name "DCGAN_CIFAR10" --data "CIFAR10" --epochs 30 --train True
+python main.py --model "DCGAN" --model-name "DCGAN_CIFAR10" --data "CIFAR10" --epochs 30 --train True
 # Testing WGAN_CIFAR10.
-python3 main.py --model "DCGAN" --model-name "DCGAN_CIFAR10" --data "CIFAR10"
+python main.py --model "DCGAN" --model-name "DCGAN_CIFAR10" --data "CIFAR10"
 
 # Training WGAN on FashionMNIST dataset.
-python3 main.py --model "DCGAN" --model-name "DCGAN_FashionMNIST" --data "FashionMNIST" --epochs 30 --train True
+python main.py --model "DCGAN" --model-name "DCGAN_FashionMNIST" --data "FashionMNIST" --epochs 30 --train True
 # Testing WGAN_FashionMNIST.
-python3 main.py --model "DCGAN" --model-name "DCGAN_FashionMNIST" --data "FashionMNIST"
+python main.py --model "DCGAN" --model-name "DCGAN_FashionMNIST" --data "FashionMNIST"
